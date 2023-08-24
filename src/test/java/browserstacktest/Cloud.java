55a9c11d17e2e587dcc61f13003b178a6b164645
seleniumphase <hetalnjadav@gmail.com>
2023-08-24 10:39:15 +0530

    test1

---
+++ src/test/java/browserstacktest/Cloud.java
@@ -0,0 +1,22 @@
+package browserstacktest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Cloud {
+	
+	@Test
+	public void testsite() throws MalformedURLException
+	{
+		MutableCapabilities cap = new MutableCapabilities();
+		WebDriver driver = new RemoteWebDriver(new URL("https:hub.browserstack.com/wd/hub"),cap);
+		driver.get("https://rahulshettyacademy.com/");
+		
+	}
+
+}
